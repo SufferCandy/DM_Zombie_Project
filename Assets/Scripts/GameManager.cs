016cd8c3f0698f702bdc356aafa72003f8f30915
SufferCandy <astra.games.zero.0@gmail.com>
2026-02-14 15:33:42 +0200

    aaaa
    aaaaa

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject selectedZombie;
+    public GameObject[] zombies;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        selectedZombie = zombies[2]
+            
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
